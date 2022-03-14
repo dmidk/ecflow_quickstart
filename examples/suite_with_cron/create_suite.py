8d97b656894f51d7a2a87f5c86999841e80914fa
Kasper Hintz <kah@dmi.dk>
2022-03-14 12:37:25 +0100

    initial files in feature/cron

---
+++ examples/suite_with_cron/create_suite.py
@@ -0,0 +1,38 @@
+import os
+import ecflow as ecf
+
+ECF_HOME=os.environ.get('ECF_HOME', '.')
+ECF_INCLUDE=os.environ.get('ECF_INCLUDE', '.')
+ECF_FILES=os.environ.get('ECF_FILES', '.')
+
+
+def create_suite(name, families:list, ecfdir:str) -> ecf.Suite:
+    suite = ecf.Suite(name)
+    suite.add_variable("ECF_INCLUDE", ECF_INCLUDE)
+    suite.add_variable("ECF_FILES", ECF_FILES)
+    suite.add_variable("ECF_HOME", ECF_HOME)
+
+
+    for family in families:
+        fam = suite.add_family(family)
+        fam.add_variable("FAMILY", 'ecf')
+        for t in ["task1", "task2"] :
+            fam.add_task(t)
+
+    return suite
+
+
+if __name__=='__main__':
+
+    print("Creating suite definition")
+
+    suite = create_suite('suiteName', ['familyName1', 'familyName2'], 'ecf')
+    defs = ecf.Defs(suite)
+
+    print(defs)
+    
+    print("Checking job creation: .ecf -> .job0")
+    print(defs.check_job_creation())
+    
+    print("Saving definition to file 'suiteName.def'")
+    defs.save_as_defs("suiteName.def")
